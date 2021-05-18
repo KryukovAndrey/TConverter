14987cbadf90bf037080ce5a6faaff5abd4da608
Kryukov Andrey <65622566+KryukovAndrey@users.noreply.github.com>
2021-05-18 15:35:05 +0300

    Initial Commit

---
+++ TConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Andrey on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var celsiumLabel: UILabel!
+    @IBOutlet weak var fahrenheitLAbel: UILabel!
+    @IBOutlet weak var slader: UISlider!
+    
+    @IBOutlet weak var sladerChanged: UISlider!
+    
+
+}
+
